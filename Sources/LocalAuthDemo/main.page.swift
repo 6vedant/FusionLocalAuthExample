0b84ef3cfc5b65baf4cb367892f1d067eabbc61d
vj <vedantj@nova.tech>
2022-03-05 11:02:06 +0530

    Initial Commit

--- Sources/LocalAuthDemo/main.page.swift
+++ Sources/LocalAuthDemo/main.page.swift
@@ -1,46 +1,41 @@
-import ScadeKit
 import FusionLocalAuth
-  
+import ScadeKit
+
 class MainPageAdapter: SCDLatticePageAdapter {
-  
+
   let localAuth: LocalAuthManager = LocalAuthManager()
-    
-  
+
   // page adapter initialization
   override func load(_ path: String) {
     super.load(path)
-    
-        
-    
+
     let bool = localAuth.canAuthenticateWithBiometrics()
     if !bool {
-    	self.label2.text = "The device can't authenticate using Biometrics!"
+      self.label.text = "The device can't authenticate using Biometrics!"
     } else {
-    	
-    	self.label2.text = "The device can authenticate with biometrics"
+
+      self.label.text = "The device can authenticate with biometrics"
     }
-    
+
     self.button.onClick { [weak self] _ in
-    print("clicked")
-    	guard let S = self else { return }
-    		
-    
-    		S.localAuth.requestBiometricAuthentication(authTitle: "authTitle", reasonTitle: "reasonTitle", cancelTitle: "cancelTitle", completionStatus: { [weak self ] (success, error) in
-    		
-    		guard success else {
-    			print(error?.description)
-    			S.label2.text = "Auth Failed"
-    			return
-    		}
-    		
-    		S.label2.text = "Auth Successful"
-    			
-    		})
-    
-    			
-    } 
-         
-         
-    
+      print("clicked")
+      guard let S = self else { return }
+
+      S.localAuth.requestBiometricAuthentication(
+        authTitle: "authTitle", reasonTitle: "reasonTitle", cancelTitle: "cancelTitle",
+        completionStatus: { [weak self] (success, error) in
+
+          guard success else {
+            print(error?.description)
+            S.label2.text = "Auth Failed"
+            return
+          }
+
+          S.label2.text = "Auth Successful"
+
+        })
+
+    }
+
   }
 }
