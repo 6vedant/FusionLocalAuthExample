6f693e37a04f603181eb95bcbb5c9b2c0d75e668
Vedant Jha <6vedant@gmail.com>
2023-04-29 21:17:25 +0530

    Updates

--- Sources/FusionLocalAuthExample/main.page.swift
+++ Sources/FusionLocalAuthExample/main.page.swift
@@ -1,16 +1,15 @@
-import ScadeKit
 import FusionLocalAuth
-  
+import ScadeKit
+
 class MainPageAdapter: SCDLatticePageAdapter {
-  
-  
+
   var localAuth: LocalAuthManager?
 
   // page adapter initialization
   override func load(_ path: String) {
     super.load(path)
-    localAuth =  LocalAuthManager()
-    
+    localAuth = LocalAuthManager()
+
     let bool = localAuth!.canAuthenticateWithBiometrics()
     if !bool {
       self.label.text = "The device can't authenticate using Biometrics!"
@@ -19,29 +18,24 @@ class MainPageAdapter: SCDLatticePageAdapter {
 
       self.label.text = "The device can authenticate with biometrics"
     }
-    
+
     localAuth!.requestBiometricAuthentication(
-        authTitle: "Please Authenticate!", reasonTitle: "This is required to access app.", cancelTitle: "Cancel authentication",
-        completionStatus: { [weak self] (success, error) in
-guard let S = self else { return }
-          guard success else {
-            print(error?.description)
-           // S.label2.text = "Auth Failed"
-           print("Authentication denied.")
-            return
-          }
-          print("Authentication successful")
+      authTitle: "Please Authenticate!", reasonTitle: "This is required to access app.",
+      cancelTitle: "Cancel authentication",
+      completionStatus: { [weak self] (success, error) in
+        guard let S = self else { return }
+        guard success else {
+          print(error?.description)
+          // S.label2.text = "Auth Failed"
+          print("Authentication denied.")
+          return
+        }
+        print("Authentication successful")
 
-          ///S.label2.text = "Auth Successful"
+        ///S.label2.text = "Auth Successful"
 
-        })
-
-
-   
-      
-
-      
-    }
+      })
 
   }
 
+}
